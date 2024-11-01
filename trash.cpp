d6612f565b6db1f12c9cd2dddb199e1080e2ee17
Mngdd <74361463+Mngdd@users.noreply.github.com>
2024-11-01 08:06:51 +0300

    Update trash.cpp

--- trash.cpp
+++ trash.cpp
@@ -1,137 +1,113 @@
-
+#define _GNU_SOURCE
 #include <stdio.h>
-#include <dirent.h>
 #include <stdlib.h>
-#include <time.h>
-#include <sys/stat.h>
-#include <sys/types.h>
 #include <unistd.h>
 #include <errno.h>
 #include <string.h>
+#include <sys/wait.h>
 
-int is_regular_file(const char* path) {
-    struct stat path_stat;
-    stat(path, &path_stat);
-    return S_ISREG(path_stat.st_mode);
-}
+const int MAX_COMMAND_AMOUNT = 256;
+const int MAX_COMMAND_LENGTH = 256;
+char commands_arr[MAX_COMMAND_AMOUNT][MAX_COMMAND_LENGTH];
 
-time_t getFileModTime(char* path) {
-    struct stat attr;
-    stat(path, &attr);
-    return attr.st_mtime;
-}
+void executeCommand(int com_id) {
+    char s[MAX_COMMAND_LENGTH];
+    strcpy(s, commands_arr[com_id]);
 
-void execute_command(char* const args[]) {
-    pid_t pid = fork();
-    if (pid == 0) {
-        // Child process executes the command
-        execvp(args[0], args);
+    char* token;
+    int cur_word = 0;
+
+    char timeout[MAX_COMMAND_LENGTH];
+    char command[MAX_COMMAND_LENGTH];
+    int com_len = 0;
+
+    // Parse timeout and command from input string
+    for (token = strtok(s, " "); token; token = strtok(NULL, " ")) {
+        switch (cur_word) {
+            case 0:  // First token is the timeout
+                strcpy(timeout, token);
+                break;
+            default: // Remaining tokens form the command
+                if (cur_word != 1) {
+                    command[com_len++] = ' ';
+                }
+                for (int i = 0; i < strlen(token); i++) {
+                    command[com_len++] = token[i];
+                }
+                break;
+        }
+        ++cur_word;
+    }
+    command[com_len] = '\0';
+
+    // Sleep for the specified timeout
+    sleep(atoi(timeout));
+
+    // Prepare the command and arguments for execvp
+    char* args[MAX_COMMAND_LENGTH];
+    int argc = 0;
+    token = strtok(command, " ");
+    while (token != NULL) {
+        args[argc++] = token;
+        token = strtok(NULL, " ");
+    }
+    args[argc] = NULL;
+
+    // Execute the command with execvp
+    if (execvp(args[0], args) == -1) {
         perror("execvp failed");
-        exit(1);
-    } else if (pid > 0) {
-        // Parent process waits for the child to finish
-        int status;
-        waitpid(pid, &status, 0);
-        if (WIFEXITED(status)) {
-            printf("Command exited with status %d\n", WEXITSTATUS(status));
-        } else {
-            printf("Command did not terminate normally\n");
-        }
-    } else {
-        perror("fork failed");
+        exit(1); // Exit child process on execvp failure
     }
-}
-
-void listFilesRecursively(char* origPath, char* backupPath) {
-    char currOrigPath[1000];
-    char currBackupPath[1000];
-    char errmsg[1000];
-
-    struct dirent* dp;
-    DIR* origDir = opendir(origPath);
-    DIR* backupDir = opendir(backupPath);
-
-    if (!origDir || !backupDir) {
-        strcpy(errmsg, "Unable to open directory: ");
-        if (!origDir) {
-            strcat(errmsg, origPath);
-            strcat(errmsg, " ");
-        }
-        if (!backupDir)
-            strcat(errmsg, backupPath);
-
-        perror(errmsg);
-        return;
-    }
-
-    while ((dp = readdir(origDir)) != NULL) {
-        if (strcmp(dp->d_name, ".") != 0 && strcmp(dp->d_name, "..") != 0) {
-            printf("%s\n", dp->d_name);
-
-            strcpy(currOrigPath, origPath);
-            strcat(currOrigPath, "/");
-            strcat(currOrigPath, dp->d_name);
-
-            strcpy(currBackupPath, backupPath);
-            strcat(currBackupPath, "/");
-            strcat(currBackupPath, dp->d_name);
-
-            if (is_regular_file(currOrigPath)) {
-                strcat(currBackupPath, ".gz");
-                _Bool need_create_file = access(currBackupPath, F_OK) != 0;
-
-                if (!need_create_file) {
-                    time_t orig_t = getFileModTime(currOrigPath);
-                    time_t backup_t = getFileModTime(currBackupPath);
-                    printf("Original %s date: %ld, Backup %s date: %ld\n", currOrigPath, orig_t, currBackupPath, backup_t);
-
-                    if (orig_t > backup_t) {
-                        printf("----------UPDATE NEEDED for %s\n", backupPath);
-
-                        char* rm_args[] = {"rm", currBackupPath, NULL};
-                        execute_command(rm_args);
-                        need_create_file = 1;
-                    }
-                }
-
-                if (need_create_file) {
-                    char* cp_args[] = {"cp", "-r", currOrigPath, backupPath, NULL};
-                    execute_command(cp_args);
-
-                    char* gzip_args[] = {"gzip", currBackupPath, NULL};
-                    execute_command(gzip_args);
-                }
-            } else {
-                DIR* bckp_test_dir = opendir(currBackupPath);
-                if (bckp_test_dir) {
-                    closedir(bckp_test_dir);
-                } else if (ENOENT == errno) {
-                    char* mkdir_args[] = {"mkdir", currBackupPath, NULL};
-                    execute_command(mkdir_args);
-                } else {
-                    closedir(bckp_test_dir);
-                    snprintf(errmsg, sizeof(errmsg), "Unable to open directory: %s, unexpected error: %s", currBackupPath, strerror(errno));
-                    perror(errmsg);
-                }
-
-                listFilesRecursively(currOrigPath, currBackupPath);
-            }
-        }
-    }
-
-    closedir(origDir);
-    closedir(backupDir);
 }
 
 int main(int argc, char** argv) {
-    if (argc != 3) {
-        fprintf(stderr, "Usage: %s SOURCE DEST\n", argv[0]);
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s FILE_NAME\n", argv[0]);
         return 1;
     }
-    
-    char* src_name = argv[1];
-    char* dest_name = argv[2];
 
-    listFilesRecursively(src_name, dest_name);
+    char* src_name = argv[1];
+    FILE* le_file = fopen(src_name, "r");
+    if (le_file == NULL) {
+        fprintf(stderr, "Couldn't open %s: %s\n", src_name, strerror(errno));
+        return EXIT_FAILURE;
+    }
+
+    char line[MAX_COMMAND_LENGTH];
+    int curr_command = 0;
+
+    // Read commands from file
+    while (fgets(line, sizeof(line), le_file)) {
+        line[strcspn(line, "\n")] = 0;  // Remove newline character
+        strcpy(commands_arr[curr_command], line);
+        curr_command++;
+    }
+    fclose(le_file);
+
+    // Fork and execute each command in a separate process
+    for (int i = 0; i < curr_command; i++) {
+        pid_t pid = fork();
+        if (pid == 0) {
+            // Child process
+            executeCommand(i);
+            exit(0);  // Exit child process after executing command
+        } else if (pid < 0) {
+            // Error in fork
+            perror("fork failed");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    // Parent process waits for all child processes to complete
+    for (int i = 0; i < curr_command; i++) {
+        int status;
+        wait(&status);  // Wait for each child process
+        if (WIFEXITED(status)) {
+            printf("Child process completed with exit status %d\n", WEXITSTATUS(status));
+        } else {
+            printf("Child process did not terminate normally\n");
+        }
+    }
+
     return 0;
 }
